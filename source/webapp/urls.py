dbac8874e2eed6fdf82caa5146e60c124cc24502
Artur <metis130383@gmail.com>
2020-09-26 15:02:56 +0600

    добавлен детальный  просмотр

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from webapp.views import ProductsView
+from webapp.views import ProductsView,ProductView
 app_name = 'webapp'
 
 
 urlpatterns = [
     path('', ProductsView.as_view(), name='index'),
+    path('product/<int:pk>/', ProductView.as_view(), name='product_view'),
 
 ]
--- source/webapp/views/product_view.py
+++ source/webapp/views/product_view.py
@@ -10,3 +10,20 @@ class ProductsView(ListView):
     template_name = 'product/index.html'
     model = Product
     context_object_name = 'products'
+
+
+class ProductView(DetailView):
+    template_name = 'product/product_view.html'
+    context_object_name = 'product'
+    model = Product
+
+
+class ProductCreateView(PermissionRequiredMixin, CreateView):
+    model = Product
+    template_name = 'product/create_view.html'
+    form_class = ProductForm
+    permission_required = 'webapp.add_product'
+    permission_denied_message = '403 Доступ запрещен'
+
+    def get_success_url(self):
+        return reverse('webapp:product_view', kwargs={'pk': self.object.pk})
